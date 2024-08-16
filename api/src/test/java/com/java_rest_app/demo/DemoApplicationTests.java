ffd152291d60596ef1978efb9914753f9ddf4d01
John Nooney <1803534@uad.ac.uk>
2024-08-16 17:03:18 +0100

    Add boilerplate for java projects

---
+++ api/src/test/java/com/java_rest_app/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_rest_app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
