0a9bca4e8ff242078bcf2dba505174e933f48eb9
John Nooney <1803534@uad.ac.uk>
2024-08-17 15:37:50 +0100

    Add adapter implementation

--- console-app/app/src/main/java/org/console/CustomerRecordAdapter.java
+++ console-app/app/src/main/java/org/console/CustomerRecordAdapter.java
@@ -5,22 +5,49 @@ import java.util.List;
 
 public class CustomerRecordAdapter {
     private List<CustomerRecord> customerList;
+    private CustomerRecordValidator validator;
 
     public CustomerRecordAdapter(){
         customerList = new ArrayList<>();
+        validator = new CustomerRecordValidator();
     }
 
-    public void createRecordsFromFileData(List<String[]> data)
+    public void createRecordsFromFileData(CSVData csvData)
     {
-        if(data.size() <= 0) {
+        List<String[]> csvList = csvData.getLinesAsList();
+
+        if(csvList.size() == 0) {
             System.out.println("No data from CSV to convert.");
             return;
         }
 
-        // TODO: convert string elements to CustomerRecords
+        csvList.forEach(csvRecord -> {
+            CustomerRecord record;
+            
+            try {
+                record = CustomerRecord.builder()
+                    .setCustomerRef(Integer.parseInt(csvRecord[0]))
+                    .setCustomerName(csvRecord[1])
+                    .setAddressLine1(csvRecord[2])
+                    .setAddressLine2(csvRecord[3])
+                    .setTown(csvRecord[4])
+                    .setCounty(csvRecord[5])
+                    .setCountry(csvRecord[6])
+                    .setPostCode(csvRecord[7])
+                    .build(); 
+        
+                validator.validateRecord(record);
+
+                customerList.add(record);
+
+                System.out.println("Adapter created record:" + String.join(",", csvRecord));
+            } catch (Exception ex) {
+                System.err.println("Adapter failed to create record:" + String.join(",", csvRecord) + " for reason: " + ex);
+            }
+        });
     }
 
-    public List<CustomerRecord> getRecords()
+    public List<CustomerRecord> getRecordsList()
     {
         return customerList;
     }
