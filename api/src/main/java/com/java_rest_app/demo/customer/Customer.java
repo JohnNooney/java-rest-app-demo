4780df342b6a84e9df90337fb088246bf395312f
John Nooney <1803534@uad.ac.uk>
2024-08-17 23:05:36 +0100

    Add customer getters/setters

--- api/src/main/java/com/java_rest_app/demo/customer/Customer.java
+++ api/src/main/java/com/java_rest_app/demo/customer/Customer.java
@@ -1,4 +1,4 @@
-package com.java_rest_app.demo;
+package com.java_rest_app.demo.customer;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -17,4 +17,68 @@ public class Customer {
     private String town;
     private String country;
     private String postcode;
+
+    public Integer getCustomerRef() {
+        return customerRef;
+    }
+
+    public void setCustomerRef(Integer customerRef) {
+        this.customerRef = customerRef;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
 }
