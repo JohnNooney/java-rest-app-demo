d5db314bd54fb091dd0fa7e3661ece2df4671486
John Nooney <1803534@uad.ac.uk>
2024-08-18 11:55:51 +0100

    Add rest controller with Get/Post

---
+++ api/src/main/java/com/java_rest_app/demo/customer/CustomerController.java
@@ -0,0 +1,28 @@
+package com.java_rest_app.demo.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/customer")
+    public ResponseEntity<Object> createCustomer(@RequestBody Customer customer) {
+        return customerService.newCustomer(customer);
+    }
+
+    @GetMapping("/customer/{customerRef}")
+    public ResponseEntity<Object> getCustomerByRef(@PathVariable Integer customerRef) {
+         return customerService.getCustomer(customerRef);
+    }
+
+
+}
--- api/src/main/java/com/java_rest_app/demo/customer/CustomerService.java
+++ api/src/main/java/com/java_rest_app/demo/customer/CustomerService.java
@@ -1,6 +1,10 @@
 package com.java_rest_app.demo.customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -8,6 +12,24 @@ public class CustomerService {
 
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
+
         this.customerRepository = customerRepository;
+    }
+
+    public ResponseEntity<Object> newCustomer(Customer customer) {
+        customerRepository.save(customer);
+
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Object> getCustomer(Integer id) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+
+        if(!customerOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Customer customer = customerOptional.get();
+        return ResponseEntity.ok(customer);
     }
 }
