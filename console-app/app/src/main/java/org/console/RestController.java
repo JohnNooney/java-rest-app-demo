16a24fd83ec6cc62eb6ed1a999051e1643d1f255
John Nooney <1803534@uad.ac.uk>
2024-08-18 15:20:00 +0100

    Add error handling and closeable httpclient instantiation

--- console-app/app/src/main/java/org/console/RestController.java
+++ console-app/app/src/main/java/org/console/RestController.java
@@ -6,18 +6,18 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 public class RestController {
-    private HttpClient httpClient;
     private String hostURI;
 
     public RestController(String hostURI) {
-        httpClient = HttpClientBuilder.create().build();
         this.hostURI = hostURI;
     }
 
@@ -29,22 +29,29 @@ public class RestController {
         HttpPost postMethod = new HttpPost(hostURI + endpoint);
         postMethod.setEntity(requestEntity);
 
-        try {
-            HttpResponse rawResponse = httpClient.execute(postMethod);
-            System.out.println("POST Response: " + rawResponse.toString());
-        } catch (Exception ex) {
-            System.out.println("POST Exception: " + ex.getMessage());
-        }
+        executeRequest(postMethod);
     }
 
     public void getById(String endpoint, Integer id) {
         HttpGet getMethod = new HttpGet(hostURI + endpoint + id.toString());
 
-        try {
-            HttpResponse rawResponse = httpClient.execute(getMethod);
-            System.out.println("GET Response: " + rawResponse.toString());
+        executeRequest(getMethod);
+    }
+
+    private void executeRequest(HttpRequestBase request) {
+        try(CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpResponse rawResponse = client.execute(request);
+            int statusCode = rawResponse.getStatusLine().getStatusCode();
+
+            if (statusCode >= 200 && statusCode < 300) {
+                HttpEntity entity = rawResponse.getEntity();
+                System.out.println(request.getMethod() + " Response: " + EntityUtils.toString(entity));
+
+            } else {
+                throw new RuntimeException(request.getMethod() + " request failed with status code: " + statusCode);
+            }
         } catch (Exception ex) {
-            System.out.println("GET Exception: " + ex.getMessage());
+            System.out.println(request.getMethod() + " Exception: " + ex.getMessage());
         }
     }
 }
